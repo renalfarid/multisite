9d137637bb5f93c5a04fe4a27d896f592bc8ef94
renalfarid <enal.farid@gmail.com>
2023-09-05 12:48:07 +0800

    add templates

--- lib/db.ts
+++ lib/db.ts
@@ -1,28 +1,28 @@
 // Dummy data to be replaced with your database
-const hostnamesDB = [
-    {
-      name: 'This is Site 1',
-      template: '001',
-      description: 'Subdomain + custom domain',
-      subdomain: 'test1',
-      customDomain: 'custom-domain-1.com',
-      // Default subdomain for Preview deployments and for local development
-      defaultForPreview: true,
-    },
-    {
-      name: 'This is Site 2',
-      template: '002',
+interface Hostname {
+    name: string;
+    template: string;
+    description: string;
+    subdomain?: string;
+    customDomain?: string;
+    defaultForPreview?: boolean;
+  }
+  
+  const hostnamesDB: Hostname[] = [];
+  const NUM_SUBDOMAINS = 10; // Change this number to the desired number of subdomains (N)
+  
+  // Generate subdomains dynamically
+  for (let i = 1; i <= NUM_SUBDOMAINS; i++) {
+    const subdomain = `test${i}`;
+    hostnamesDB.push({
+      name: `This is Site ${i}`,
+      template: `00${i}`,
       description: 'Subdomain only',
-      subdomain: 'test2',
-    },
-    {
-      name: 'This is Site 3',
-      template: '003',
-      description: 'Subdomain only',
-      subdomain: 'test3',
-    },
-  ]
-  const DEFAULT_HOST = hostnamesDB.find((h) => h.defaultForPreview)
+      subdomain,
+    });
+  }
+  
+  const DEFAULT_HOST = hostnamesDB.find((h) => h.defaultForPreview);
   
   /**
    * Returns the data of the hostname based on its subdomain or custom domain
@@ -32,20 +32,20 @@ const hostnamesDB = [
    */
   export async function getHostnameDataOrDefault(
     subdomainOrCustomDomain?: string
-  ) {
-    if (!subdomainOrCustomDomain) return DEFAULT_HOST
+  ): Promise<Hostname> {
+    if (!subdomainOrCustomDomain) return DEFAULT_HOST as Hostname;
   
     // check if site is a custom domain or a subdomain
-    const customDomain = subdomainOrCustomDomain.includes('.')
+    const customDomain = subdomainOrCustomDomain.includes('.');
   
-    // fetch data from mock database using the site value as the key
+    // fetch data from the mock database using the site value as the key
     return (
       hostnamesDB.find((item) =>
         customDomain
           ? item.customDomain === subdomainOrCustomDomain
           : item.subdomain === subdomainOrCustomDomain
-      ) ?? DEFAULT_HOST
-    )
+      ) ?? (DEFAULT_HOST as Hostname)
+    );
   }
   
   /**
@@ -53,22 +53,25 @@ const hostnamesDB = [
    *
    * This method is used by pages under middleware.ts
    */
-  export async function getHostnameDataBySubdomain(subdomain: string) {
-    return hostnamesDB.find((item) => item.subdomain === subdomain)
+  export async function getHostnameDataBySubdomain(
+    subdomain: string
+  ): Promise<Hostname | undefined> {
+    return hostnamesDB.find((item) => item.subdomain === subdomain);
   }
   
   /**
    * Returns the paths for `getStaticPaths` based on the subdomain of every
    * available hostname.
    */
-  export async function getSubdomainPaths() {
+  export async function getSubdomainPaths(): Promise<{ params: { site: string } }[]> {
     // get all sites that have subdomains set up
-    const subdomains = hostnamesDB.filter((item) => item.subdomain)
+    const subdomains = hostnamesDB.filter((item) => item.subdomain);
   
     // build paths for each of the sites in the previous two lists
     return subdomains.map((item) => {
-      return { params: { site: item.subdomain } }
-    })
+      return { params: { site: item.subdomain as string } };
+    });
   }
   
-  export default hostnamesDB
+  export default hostnamesDB;
+  
